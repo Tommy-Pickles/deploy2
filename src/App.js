8378ab207324f81fd97d571feb97e2799fc084fe
Tommy Pickles <jhawke2546@gmail.com>
2022-09-26 19:24:55 +1000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import profileImage from './img/img-9-1.jpg'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> Hello World </h1>
+      <h2>Made by MIke</h2>
+      < img src={profileImage} alt='profile-pic' />
     </div>
   );
 }
