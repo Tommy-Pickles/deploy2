8d5e1201ee7f7378fa574a143a0defdcf1f0061c
Tommy Pickles <jhawke2546@gmail.com>
2022-09-28 08:32:43 +1000

    lol

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
-import profileImage from './img/joel.jpg'
+import profileImage from './img/no-pixels.jpg'
 
 function App() {
   return (
     <div className="App">
-      <h1> Joel Loves cock </h1>
-      <h2> "Dont call me I'm BUSY"</h2>
+      <h1> OPTIs PAGE </h1>
+      <h2> "Doge King smells weird"</h2>
       < img src={profileImage} alt='profile-pic' />
     </div>
   );
